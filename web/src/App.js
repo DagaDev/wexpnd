7f66c9ea837ba8883318fdcacba5a6572e560aca
Waldo <wespinoza.innobyte>
2021-03-05 17:44:11 -0600

    react router

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
 function App() {
     return (
-        <div className="container-fluid bg-primary">
-            fdd
-        </div>
+        <Router>
+            <div className="container-fluid bg-primary">
+                fddd
+            </div>
+        </Router>
     );
 }
 
