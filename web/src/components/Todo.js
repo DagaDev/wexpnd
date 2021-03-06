2cedf1267172821b326ca2fb9e88a4c876a6af29
Waldo <waldodev@hotmail.com>
2021-03-06 11:58:08 -0600

    acomod de vista

--- web/src/components/Todo.js
+++ web/src/components/Todo.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ListaTodo from './ListaTodo';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
 const Todo = () => {
 
+    const useStyles = makeStyles((theme) => ({
+        button: {
+          margin: theme.spacing(1),
+        },
+    }));
+    const classes = useStyles();
     return(
-        <div className="d-flex justify-content-md-center col-lg-12 h-100 vh-100">
-            <div className="col-md-6 align-self-center" >
+        
+        <div className="d-flex mlr h-100 vh-100">
+            <div className="container-fluid card">
+                <div className="d-flex justify-content-between">
+                    <h2>TODO</h2>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        className={classes.button}
+                        startIcon={<AddIcon />}
+                    >
+                        Agregar
+                    </Button>
+                </div>
                 <ListaTodo />
             </div>
         </div>
